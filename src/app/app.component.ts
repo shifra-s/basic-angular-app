4bb2ac0e93115d04e62acd3b8ef5c1f2391863ae
shifra <shif602@gmail.com>
2019-02-14 19:39:03 +0200

    implemented logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'basic-app';
+
+// export class AppComponent {
+//   title = 'basic-app';
+// }
+
+export class AppComponent implements OnInit {
+
+  isLogged: boolean;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.isLogged().subscribe(res => {
+      this.isLogged = res;
+    })
+
+  }
+
+
+  logout() {
+    this.authService.logout();
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { of, throwError, Observable } from 'rxjs';
+import { of, throwError, Observable, BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment.prod';
 
@@ -8,13 +8,20 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class AuthService {
 
-  constructor() { }
+//internal parameter in the service
+  private _isLogged: BehaviorSubject<boolean>;
+
+  constructor() {
+    this._isLogged = new BehaviorSubject<boolean>(false);
+   }
 
   login(email: string, password:string): Observable<string>{
     if (email=='a@a.com' && password=='aaa') {
       return of('7t78t7t8vyfuygjhjj').pipe(
         map(res => {
           this.setToken(res);
+          //the value of islogged is true
+          this._isLogged.next(true);
           return res;
         })
       )} 
@@ -32,9 +39,20 @@ export class AuthService {
     return window.localStorage.getItem(environment.tokenKey);
   }
 
-//tell me if user is logged in
-//should be observable of...
+  //logout - delete the token
+  private deleteToken(){
+    window.localStorage.removeItem(environment.tokenKey);
+  }
+
+//returns whether user is logged in
   isLogged():Observable<boolean> {
-    return of(this.getToken()!= null);
+    return this._isLogged;
+    // return of(this.getToken()!= null);
+  }
+
+  logout(){
+    //user is logged out - so set to false
+    this._isLogged.next(false);
+    this.deleteToken();
   }
 }
